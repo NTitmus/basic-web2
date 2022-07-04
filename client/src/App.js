66e319e1a8a1229e126e4ba6041399a05ff481b0
Nicola Titmus <nicola.koo@cantab.net>
2022-07-04 23:04:32 +0100

    display api message and counter

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import { useState } from 'react/cjs/react.production.min';
+
+const [mess, setMess] = useState("Start")
+const [count, setCount] = useState(0)
 
 const onButtonClick = () => {
   const callAPI = async () => {
     const basicResponse = await axios.get('/api/message');
     console.log(basicResponse)
+    setMess(basicResponse)
+    setCount(count + 1)
   }
   callAPI();  
 }
@@ -15,6 +21,11 @@ function App() {
     <div>
       <h1>Hi there! ***</h1>
       <button onClick={()=>onButtonClick()}>Call API</button>
+      <div>
+        <h2>0!</h2>
+        <p>{mess}</p>
+        <h3>{count}</h3>
+      </div>
     </div>
   );
 }
