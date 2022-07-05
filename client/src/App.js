ce8594262a8875e72f007c97fbc1079d781d3985
Nicola Titmus <nicola.koo@cantab.net>
2022-07-05 08:59:06 +0100

    added 2nd button

--- client/src/App.js
+++ client/src/App.js
@@ -8,24 +8,36 @@ import { useState } from 'react';
 
 
 
-function App() {
+const App = () => {
   const [mess, setMess] = useState('Start');
   const [count, setCount] = useState(0)
 
-  const onButtonClick = () => {
-    const callAPI = async () => {
-      const basicResponse = await axios.get('/api/message');
-      console.log(basicResponse)
-      setMess(basicResponse)
-      setCount(count + 1)
-    }
-    callAPI();  
+  
+const onButtonClick = () => {
+  const callAPI = async () => {
+    const basicResponse = await axios.get('/api/message');
+    console.log(basicResponse)
+    setMess(basicResponse.data)
+    setCount(count + 1)
   }
+  callAPI();  
+}
+
+const onButton2Click = () => {
+  const callAPI2 = async () => {
+    const basicResponse2 = await axios.get('/api/alternative');
+    console.log(basicResponse2.data);
+    setMess(basicResponse2.data);
+    setCount(count + 1);
+  }
+  callAPI2()
+}
 
   return (
     <div>
-      <h1>Hi there! ***</h1>
+      <h1>Hi there....! ***</h1>
       <button onClick={()=>onButtonClick()}>Call API</button>
+      <button onClick={()=>onButton2Click()}>SecondButton</button>
       <div>
         <h2>0!</h2>
         <p>{mess}</p>
